0cc2a2872a51cfd4c1d02a975a3baf9a4834d187
Jonathan Sick <jonathansick@mac.com>
2012-10-18 00:02:10 +0000

    Add remote tracking branch to prompt

--- prompts/jsick_vcs_info.zsh
+++ prompts/jsick_vcs_info.zsh
@@ -3,22 +3,21 @@
 autoload -U colors && colors
 autoload -Uz vcs_info
 
-zstyle ':vcs_info:*+*:*' debug true
+# zstyle ':vcs_info:*+*:*' debug true
 zstyle ':vcs_info:*' enable git
 zstyle ':vcs_info:git*:*' get-revision true
 zstyle ':vcs_info:git*:*' check-for-changes true
-# zstyle ':vcs_info:git*+set-message:*' hooks git-st git-stash
+zstyle ':vcs_info:git*+set-message:*' hooks git-untracked git-st git-stash git2symbol
 
 
 # Formats for git vcs_info
 # see http://zsh.sourceforge.net/Doc/Release/User-Contributions.html#Version-Control-Information
 # for syntax
-zstyle ':vcs_info:git*' formats "%r %.7i %b
+zstyle ':vcs_info:git*' formats "%r %.7i %b%m
 %s"
 
 # Lets me customize the vcs_message for git
 # Based on the vcs_info docs, search svn2subversion
-zstyle ':vcs_info:git+set-message:*' hooks git2symbol
 function +vi-git2symbol() {
     hook_com[vcs]=±
 }
@@ -30,10 +29,27 @@ function +vi-git-stash() {
 
     if [[ -s ${hook_com[base]}/.git/refs/stash ]] ; then
             stash_count=$(git stash list 2>/dev/null | wc -l)
-            hook_com[misc]+=" (${stashe_count} stashed)"
+            hook_com[misc]+=" (${stash_count} stashed)"
     fi
 }
 
+# Show remote ref name and number of commits ahead-of or behind
+# https://raw.github.com/whiteinge/dotfiles/master/.zsh_shouse_prompt
+# This needs git 1.7+
+function +vi-git-st() {
+    local ahead behind remote
+    local -a gitstatus
+    remote=${$(git rev-parse --verify ${hook_com[branch]}@{upstream} \
+            --symbolic-full-name --abbrev-ref 2>/dev/null)}
+    # Are we on a remote-tracking branch?
+    if [[ -n ${remote} ]] ; then
+        ahead=$(git rev-list ${hook_com[branch]}@{upstream}..HEAD 2>/dev/null | wc -l)
+                (( $ahead )) && gitstatus+=( "+${ahead}" )
+        behind=$(git rev-list HEAD..${hook_com[branch]}@{upstream} 2>/dev/null | wc -l)
+                (( $behind )) && gitstatus+=( "-${behind}" )
+        hook_com[branch]="${hook_com[branch]} [${remote} ${(j:/:)gitstatus}]"
+    fi
+}
 
 # Indicate if there are any untracked files present
 # https://raw.github.com/whiteinge/dotfiles/master/.zsh_shouse_prompt

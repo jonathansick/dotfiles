3d8392bdb58a79aa4b32ed371c801d6bafb361ba
Jonathan Sick <jonathansick@mac.com>
2012-11-10 16:27:31 -0500

    Add homebrew aliases
    - Seth Brown's `brewer` update-everything alias
    - a `brews` alias to list all brews

---
+++ zsh/homebrew.zsh
@@ -0,0 +1,6 @@
+# Aliases for Homebrew, the Mac package manager
+
+# From oh-my-zsh:
+alias brews='brew list -1'
+# Good one from Seth Brown: http://cl.ly/image/1A073P3n1O00
+alias brewer='brew update && brew upgrade && brew cleanup && brew doctor'
